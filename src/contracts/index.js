7bf6381d5094c2bd88627267cd684b715a9cc291
apollo0102 <crazyprince0012@gmail.com>
2022-03-14 14:03:24 -0400

    fd

--- src/contracts/index.js
+++ src/contracts/index.js
@@ -1,11 +1,8 @@
-// export const ContractAddressByRinkeby = "0xed2c22bcc9a973e33956eab2b1435b5b0b1d7f15";
+// export const ContractAddressByRinkeby = "0x322447a817fbdf0441B0381904327b240DC2Cc4E";
 // // export const StakingContractAddress = "0x656e399eB324E36c69bBFc11783f028678239f4A";
-// export const StakingContractAddress = "0x94689278f6a88ba3edE95d2FdF55a9ca0b947d02";
-// export const TokenContractAddress = "0x0dC823005657B1B7Df939c1Ae1603e7438B722Fc";
-// export const ContractAddress = "0x7c1d5E50531a625764fe6eA809375c0c216ef0BA"
+// export const StakingContractAddress = "0xE08AD1554Ef11C1CaC38BBe284d3E2Da920De611";
+// export const TokenContractAddress = "0xEb1bBA47143e67e82Dac98baE79C71f39D466F0D";
 
-
-export const ContractAddressByRinkeby = "0x322447a817fbdf0441B0381904327b240DC2Cc4E";
-// export const StakingContractAddress = "0x656e399eB324E36c69bBFc11783f028678239f4A";
-export const StakingContractAddress = "0xE08AD1554Ef11C1CaC38BBe284d3E2Da920De611";
-export const TokenContractAddress = "0xEb1bBA47143e67e82Dac98baE79C71f39D466F0D";
+export const ContractAddressByRinkeby = "0x7c1d5E50531a625764fe6eA809375c0c216ef0BA"
+export const StakingContractAddress = "0xf66c972f87926e6688b6931497202681ad483a4f";
+export const TokenContractAddress = "0xa6e74a7c8a921162c203617f4da24b512d0a7681 ";
